7e2ddba0c60e1c74f1871231ebf41e3e054edadf
Lucas Macori <inscriptions@lucasmacori.fr>
2019-03-24 21:42:32 +0100

    Created the employee form

--- src/controllers/employee.controller.ts
+++ src/controllers/employee.controller.ts
@@ -2,6 +2,26 @@ import {Request, Response} from 'express';
 import {getManager, Repository} from "typeorm";
 import {Employee} from "../entities/employee.entity";
 
+export function viewEditEmployee(req: Request, res: Response): void {
+    const id = req.params['id'];
+    if (!isNaN(id)) {
+        const employeesRepository: Repository<Employee> = getManager().getRepository(Employee);
+        employeesRepository.findOne(id)
+            .then(
+                (employee: Employee) => {
+                    res.render('employeeForm', {employee: employee});
+                }
+            )
+            .catch(
+                (err: string) => {
+                    // TODO: Catch the error, you lazy cunt
+                }
+            );
+    } else {
+        // TODO: Catch the error, you lazy cunt
+    }
+}
+
 export function getAllEmployees(res: Response): void {
     const employeesRepository: Repository<Employee> = getManager().getRepository(Employee);
     employeesRepository.find()
--- src/routes/employee.routes.ts
+++ src/routes/employee.routes.ts
@@ -1,10 +1,16 @@
 import {Request, Response, Router} from 'express';
 // Controller
-import {getAllEmployees} from "../controllers/employee.controller";
+import {getAllEmployees, viewEditEmployee} from "../controllers/employee.controller";
 
 const employeeRoutes = new Router()
     .get('/', (req: Request, res: Response) => {
         res.render('employees');
+    })
+    .get('/new', (req: Request, res: Response) => {
+        res.render('employeeForm', {id: null});
+    })
+    .get('/:id', (req: Request, res: Response) => {
+        viewEditEmployee(req, res);
     })
     .get('/all', (req: Request, res: Response) => {
         getAllEmployees(res);
